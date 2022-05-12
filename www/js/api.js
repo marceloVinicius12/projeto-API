dfea954be89c7c45e6e5ef6937f628978ac5a010
marcelo vinicius <viniciusmarcello12@gmail.com>
2022-05-11 21:35:42 -0300

    adicionando api

---
+++ www/js/api.js
@@ -0,0 +1,25 @@
+$("#btn").on("click", function(){
+    var validacep = /^[0-9]{8}$/;
+    var numCep = $("#CEP").val();
+    var url = "https://viacep.com.br/ws/"+numCep+"/json";
+    document.querySelector("#numero").innerHTML = numero
+    numero.classList.add("paciente-invalido")
+    
+    $.ajax({
+        url: url,
+        type: "get",
+        dataType: "json",
+
+        success:function(dados){
+            $("#logradouro").val(dados.logradouro);
+            $("#ddd").val("ddd "+dados.ddd);
+            $("#uf").val(dados.uf);
+            $("#cidade").val(dados.localidade);
+            $("#bairro").val(dados.bairro);
+     
+        }
+    }) 
+})
+
+
+
